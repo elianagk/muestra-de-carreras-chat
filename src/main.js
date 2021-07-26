9350a4912f328ea93f7465a748eb9255a86ccb2b
elikohon <elikohon@gmail.com>
2021-07-26 12:33:03 -0300

    vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
 Vue.config.productionTip = false
+Vue.component('ValidationObserver', ValidationObserver);
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: function (h) { return h(App) }
 }).$mount('#app')
