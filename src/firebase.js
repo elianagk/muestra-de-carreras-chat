32bd9989123b5022722ee386d8f049c5e04acae4
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-07-26 12:47:21 -0300

    Configuracion firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/analytics';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAH0Ase4QWtwT8xdFp0c3bvK98UI6j0bnY",
+    authDomain: "chat-muestra.firebaseapp.com",
+    projectId: "chat-muestra",
+    storageBucket: "chat-muestra.appspot.com",
+    messagingSenderId: "502634347735",
+    appId: "1:502634347735:web:72ec28fbb394d3d791c492",
+    measurementId: "G-7V8Z7K2W1Q"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
