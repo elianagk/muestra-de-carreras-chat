2b3913f8cd1d517792124bcaebb1f3b1c4ca33fd
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-07-26 15:23:49 -0300

    Messages

---
+++ src/components/messages/Chat.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-list-item :key="chat.id">
+        <v-list-item-avatar>
+            <v-img :src="avatar"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+            <v-list-item-title>{{senderName}}</v-list-item-title>
+            <v-list-item-subtitle v-html="chat.message"></v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>            
+            <v-list-item-action-text v-text="datetime"></v-list-item-action-text>
+        </v-list-item-action>
+    </v-list-item>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex';
+import moment from 'moment';
+export default {
+    name: "Chat",
+    props: ["chat"],
+    computed: {
+        ...mapState('roomModule', {
+            users: state => state.users
+        }),
+        sender: function() {
+            var users = this.users.filter((val) => {
+                return val.id == this.chat.senderID;
+            });
+            return users.length > 0 ? users[0] : {};
+        },
+        avatar: function() {
+            return this.sender ? this.sender.photoUrl : "";
+        },
+        senderName: function() {
+            return this.sender ? this.sender.name : "Unknown";
+        },
+        datetime: function() {
+            return moment(this.chat.timestamp.toDate()).format("h:mm a, Do MMM YYYY");
+        }
+    },
+    mounted() {
+        var element = document.getElementById("chat-list-container");
+        if (typeof(element) != 'undefined' && element != null) {
+            element.scrollTop = element.scrollHeight;
+        }
+    }
+}
+</script>
---
+++ src/components/messages/ChatInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container class="chat-input">
+        <v-textarea
+          v-model.trim="message"
+          v-on:keyup.enter.exact="submitHandler"
+          class="message"
+          solo
+          no-resize
+          rows="2"
+          name="message"
+          label="Type 'Enter' to send message"
+        ></v-textarea>
+    </v-container>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+    name: "ChatInput",
+    data () {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+        ...mapActions('roomModule', ['sendMessage']),
+        async submitHandler(e) {
+            if(this.message.length > 0) {
+                const {message} = this;
+                this.sendMessage({message}); 
+            }   
+            this.resetMessage();        
+        },
+        resetMessage() {
+            this.message = '';
+        }
+    }
+}
+</script>
