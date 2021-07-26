ce310c2dcd7c9212dab95efcf8cee2b4582041ce
elikohon <elikohon@gmail.com>
2021-07-26 14:52:02 -0300

    App.vue actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      clipped-left
+      clipped-right
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-app-bar-nav-icon class="mr-2" @click.stop="leftdrawer = !leftdrawer"></v-app-bar-nav-icon>
+        <v-img
+          :src="require('./assets/logo.png')"
+          alt="Real time chat"
+          class="shrink mr-2"
+          contain
+          transition="scale-transition"
+          width="30"
+        />
+        <h4>Real time chat</h4>
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="rightdrawer = !rightdrawer">
+          <v-icon>contacts</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="leftdrawer"
+        app
+        clipped
+        light
+    >
+        <v-list
+            dense
+            nav
+            class="py-0"
+            >
+            <Avatar />
+            <div class="overline mt-2 mb-2">Recent chats</div>
+            <RoomList v-if="!isRequiresLogin" />
+        </v-list>
+        <RoomDialog />
+        
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+        v-model="rightdrawer"
+        app
+        clipped
+        right
+        light
+    >
+      <div class="contacts-container">
+          <h3 class="overline">All contacts</h3>
+
+          <v-text-field
+              v-model="search"
+              outlined
+              dense
+              label="Search"
+              prepend-inner-icon="search"
+              class="mt-2"
+          ></v-text-field>
+          <ContactList :searchInput="search" />            
+      </div>
+    </v-navigation-drawer>
+
+    <LoginDialog />
+
+    <v-content>
+      <MessageContainer />
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapActions } from 'vuex';
+import LoginDialog from './components/login/LoginDialog'
+import ContactContainer from  './components/contacts/ContactContainer'
+import MessageContainer from './components/messages/MessageContainer'
+import Avatar from './components/Avatar'
+import RoomList from './components/rooms/RoomList'
+import RoomDialog from './components/rooms/RoomDialog'
+import ContactList from './components/contacts/ContactList'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LoginDialog,
+    ContactContainer,
+    MessageContainer,
+    Avatar, 
+    RoomList, 
+    RoomDialog,
+    ContactList
+  },
+  computed: {
+    ...mapState('userModule', {
+        userState: state => state.user
+    }),
+    isRequiresLogin: function() {
+        return !this.userState || Object.keys(this.userState).length === 0;
+    }
+  },
+  data: () => ({
+    leftdrawer: null,
+    rightdrawer: null,
+    search: ''
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    .contacts-container {
+        padding: 12px 18px;
+    }
 </style>
