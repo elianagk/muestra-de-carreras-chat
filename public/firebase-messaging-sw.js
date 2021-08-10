b3a7f113834017087476b57879409398021d5a05
CarolinaSiracusa <54592916+CarolinaSiracusa@users.noreply.github.com>
2021-08-10 01:11:45 -0300

    Cambios sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,8 +2,8 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.0/firebase-messaging.js');
 
 firebase.initializeApp({
   messagingSenderId: '502634347735'
